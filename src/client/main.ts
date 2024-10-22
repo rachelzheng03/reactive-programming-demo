f9534af94b607946f90e38b33465d0d3085c96db
RachelZheng <rzhe2003@gmail.com>
2024-10-22 09:45:39 -0400

    establish pipeline and basic display functionality

---
+++ src/client/main.ts
@@ -0,0 +1,48 @@
+import {fromEvent, map, tap, merge, shareReplay} from "rxjs";
+
+const form = document.getElementById("form")!;
+const userMessages$ = fromEvent<FormDataEvent>(form, 'submit');
+
+userMessages$.pipe(
+    // First, prevent the default form submission behavior
+    tap((e: Event) => {
+      e.preventDefault();
+      console.log('Form submission prevented.');
+    }),
+  
+    // Extract the message from the input field
+    map((e: Event) => {
+        const messageInput: HTMLInputElement = ((e.currentTarget as HTMLFormElement).querySelector('input[name="message"]')!);
+        const message = messageInput.value;
+        messageInput.value = ""; /*Note: this is a side-effect!*/
+        return message;
+    }),
+  
+    // Add additional context to the message (time, action)
+    map((message: string): Message => {
+      return {
+        data: message,
+        action: 'sent',
+        timestamp: new Date()
+      };
+    }),
+  
+    // Debugging: log the message object to the console
+    tap((message: Message) => {
+      console.log('Message sent:', message);
+    })
+  ).subscribe(message => {
+    console.log("message", message)
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = 
+    `<div>
+        <p class="message-text">${message.data}</p>
+        <p class="message-date">${message.action} ${new Date(message.timestamp).toLocaleString()}</p>
+    </div>`;
+    newMessage.classList.add(message.action);
+    document.getElementById("messages")!.appendChild(newMessage);
+});
+
+
+
+
