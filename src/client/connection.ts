f4bb1f5a9117a27de3768a43ee7858fb344bbb0d
RachelZheng <rzhe2003@gmail.com>
2024-10-22 23:42:07 -0400

    made connection.ts visible

---
+++ src/client/connection.ts
@@ -0,0 +1,20 @@
+import {Subject} from 'rxjs';
+import {io} from 'socket.io-client';
+
+export const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
+
+export const serverMessages$ = new Subject<Message>();
+
+export const sendMessage = (message: Message) => {
+    socket.emit('message', message);
+};
+
+socket.on('message', (message) => {
+    console.log('Received message:', message);
+    message.action = "received";
+    serverMessages$.next(message as Message);
+});
